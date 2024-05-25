5e9a5d85863af7699bab8614d600acadc51a3826
mariusjacob-batstateu <21-07121@g.batstate-u.edu.ph>
2024-05-25 22:24:33 +0800

    done concurrence of front end backend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,13 @@
-'use client'
-
+"use client"
 import Image from "next/image";
 import { useEffect, useState } from 'react';
 
 
 export default function Home() {
-  const[data, setData]= useState([])
+  const[data, setData]= useState(null)
 
   const fetchPosts= async()=>{
-    const response = await fetch('http://localhost:4000');
+    const response = await fetch('/api');
     const result = await response.json();
     if(response.ok){
       console.log("success");
@@ -23,7 +22,7 @@ export default function Home() {
 
   return (
     <main>
-    {data && data.map((post:any)=>(
+    {data && (data as any[]).map((post:any)=>(
       <div key={post._id}>
         <h1>{post.title}</h1>
         <p>{post.body}</p>
