9057c2d6b06600757b8470833d875b04d4ad3d6b
mariusjacob-batstateu <21-07121@g.batstate-u.edu.ph>
2024-05-26 22:46:06 +0800

    product card somehow done, needs some further styling

--- frontend/src/app/components/Trending.tsx
+++ frontend/src/app/components/Trending.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Image from 'next/image'
 
 import { Card, CardContent } from "@/components/ui/card"
 import {
@@ -8,34 +9,45 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import { Button } from "@/components/ui/button"
 
 async function fetchProducts(){
-    const res = await fetch('https://dummyapi.io/data/v1/post?limit=8', {
-        headers: {
-          'app-id': "6653052937fc48fe0534527c",
-        },
-      });
+    const res = await fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=10');
     return await res.json()
-
 }
-
 export async function Trending() {
     const products= await fetchProducts()
-
   return (
-    <Carousel className="w-full">
-      <CarouselContent className="-ml-1 bg-primary">
+    <Carousel className="w-full grid gap-7">
+      <CarouselContent className="-ml-1">
           
             
-                {products.data.length ===0?(
+                {products.length ===0?(
                     <div>Loading</div>
                 ):(
-                    products.data.map((product:any) =>(
-                        <CarouselItem key={product.id} className="pl-1 md:basis-1/2 lg:basis-1/3">
-                            <div className="p-1">
-                                <Card>
-                                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                                    <span className="text-2xl font-semibold">{product.owner.firstName}</span>
+                    products.map((product:any) =>(
+                        <CarouselItem key={product.id} className="pl-1 sm:basis-1/3  md:basis-1/4 lg:basis-1/5 bg-primary">
+                            <div className="p-1 grid h-full">
+                                <Card className="">
+                                    <CardContent className="">
+                                      <Image 
+                                      src={product.images[0]} 
+                                      alt={product.title}
+                                      height={50}
+                                      width={50}
+                                      quality={50}
+                                      priority={true}
+                                      />
+                                      
+                                    <div className="grid">
+                                      <p>{product.title}</p>
+                                      <p>P{product.price}</p>
+                                    </div>
+
+                                    <div className="flex justify-around">
+                                      <Button>Add to Cart</Button>
+                                      <Button>See Flex</Button>
+                                    </div>
                                     </CardContent>
                                 </Card>
                             
